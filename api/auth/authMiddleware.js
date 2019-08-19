9898efc018b3e9af8892b627540eec4110176f12
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-19 13:31:25 -0400

    Create middleware to check user for register and login

--- api/auth/authMiddleware.js
+++ api/auth/authMiddleware.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcryptjs');
 const Users = require('../users/usersModel');
 
-module.exports = restricted;
+module.exports = { 
+    restricted,
+    checkUserAtReg,
+    checkUserAtLogin
+};
 
 function restricted(req, res, next) {
     const { username, password } = req.headers;
@@ -18,4 +22,27 @@ function restricted(req, res, next) {
         next({ stat: 400, message: 'Please send valid credentials.' });
     }
     next();
+}
+
+async function checkUserAtReg(req, res, next) {
+    const { username, password } = req.body
+    try{
+        const storedUser = await Users.findByUsername(username)
+        if(!username || !password)
+            next({stat: 400, message: 'Please include a username and password.'})
+        else if(username === storedUser.username)
+            next({stat: 401, message: 'Username is already in use, please choose another.'})
+        else 
+            next()
+    }catch (err){
+        next({err, stat: 500, message: 'Could not register user'})
+    }
+}
+
+function checkUserAtLogin(req, res, next){
+    const { username, password }
+    if(!username || !password)
+        next({stat: 400, message: 'Please include a username and password'})    
+    else
+        next()
 }
--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const Users = require('../users/usersModel');
+const {checkUserAtReg, checkUserAtLogin } = require('./authMiddleware')
 const router = express.Router();
 
-router.post('/register', (req, res, next) => {
-    const { user } = req.body;
-    const hash = bcrypt.hashSync(user.password, 12);
-    user.password = hash;
-    Users.add(user).then(([id]) => U;
+router.post('/register', checkUserAtReg, (req, res, next) => {
+    try {
+        const user = req.body;
+        const hash = bcrypt.hashSync(user.password, 12);
+        user.password = hash;
+        const newUser = Users.add(user);
+        res.status(201).json(newUser);
+    } catch (err) {
+        next({ err, stat: 500, message: 'Could not register user.' });
+    }
 });
 
-router.post('/login', (req, res, next) => {});
+router.post('/login', checkUserAtLogin, (req, res, next) => {});
 module.exports = router;
